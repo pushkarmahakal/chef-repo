a301f0f498b5aaf419b18f93ee786164bc1c17be
Pushkar Mahakal <pushkar.mahakal@salesify.com>
2016-10-14 19:43:47 +0000

    commit 3

--- mycookbook/apache/recipes/base-files.rb
+++ mycookbook/apache/recipes/base-files.rb
@@ -1,7 +1,11 @@
 # Add file for apache to serve
 
-cookbook_file "/var/www/html/index.html" do
+cookbook_file "/var/www/index.html" do
   source "index.html"
   mode "0644"
+end
+
+service "apache2" do
+action :restart
 end
 
