57485b20bc0b686fea84fdce0f0cd615ef8bba15
Pushkar Mahakal <pushkar.mahakal@salesify.com>
2016-10-14 18:09:31 +0000

    1st commit

---
+++ mycookbook/tomcat-graylog/metadata.rb
@@ -0,0 +1,7 @@
+name             'tomcat-graylog'
+maintainer       'YOUR_COMPANY_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures tomcat-graylog'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
