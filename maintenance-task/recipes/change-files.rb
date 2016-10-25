ae6c2e792fe704dbeca9ebe401f23573a5a2907c
Pushkar Mahakal <pushkar.mahakal@salesify.com>
2016-10-25 20:59:53 +0000

    commit 3b

--- maintenance-task/recipes/change-files.rb
+++ maintenance-task/recipes/change-files.rb
@@ -7,11 +7,3 @@ ruby_block "insert_line" do
   end
 end
 
-ruby_block "insert_line" do
-  block do
-    file = Chef::Util::FileEdit.new("/etc/rsyslog.conf")
-    file.insert_line_if_no_match("\\$\\IncludeConfig1 /etc/rsyslog.d/\\*\\.conf", "$IncludeConfig1 /etc/rsyslog.d/*.conf")
-    file.write_file
-  end
-end
-
