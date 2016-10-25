bc652fee343644951a90e3bf07d0ce7b34131fb7
Pushkar Mahakal <pushkar.mahakal@salesify.com>
2016-10-25 20:28:31 +0000

    commit 3a

--- maintenance-task/recipes/set-hostname.rb
+++ maintenance-task/recipes/set-hostname.rb
@@ -1,5 +1,6 @@
 #Change hostname of instance
 execute "set hostname" do
-  command "hostname  <%= node['ec2']['placement_availability_zone'] %>"
+  command "hostname test.node.es1"
+#  command "hostname  <%= node['ec2']['placement_availability_zone'] %>"
 end
 
